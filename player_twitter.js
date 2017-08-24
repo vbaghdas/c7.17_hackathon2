a1f8f616af959cd0737181549c6b04c9fd9b3122
cbryant24 <chrs.bryant@gmail.com>
2017-08-24 13:10:26 -0700

    final version

--- player_twitter.js
+++ player_twitter.js
@@ -101,10 +101,7 @@ this.callSuccess = function(response){
 	});
 	var $name = $('<div>',{
 		text: response.info.name,
-		class: 'tweetName',
-		css: {
-			'font-size': '1em'
-		}
+		class: 'tweetName'
 	});
 	var $tweet = $('<div>',{
 		class: 'twitterPost'
